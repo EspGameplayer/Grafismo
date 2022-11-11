ac94e19660c08942953e8467e9263d84392e0c98
José Antonio <jadg1999@gmail.com>
2022-11-11 23:08:18 +0100

    Project

---
+++ src/main/java/grafismo/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package grafismo.repository;
+
+import grafismo.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
---
+++ src/main/java/grafismo/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package grafismo.service.dto;
